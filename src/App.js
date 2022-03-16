7b3798329b2890a796e0c54c8472c496d0b3304e
hasan-mohammad-bd <hasan.md.east@gmail.com>
2022-03-16 08:13:36 +0600

    what the hell is going on

--- src/App.js
+++ src/App.js
@@ -6,9 +6,13 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>JSX</h1>
+        <div className="container"></div>
+        <h3>Hello dude. react . how are you</h3>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        
         <a
           className="App-link"
           href="https://reactjs.org"
